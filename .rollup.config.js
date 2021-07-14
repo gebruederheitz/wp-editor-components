6d012928a5d92746ca4a756607c3628568276250
AndreasMaros <andreas.maros@gebruederheitz.de>
2021-07-14 10:31:48 +0200

    [fix] Improved builds with babel runtime and core-js

--- .rollup.config.js
+++ .rollup.config.js
@@ -2,34 +2,45 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 
-export default {
-    external: ['wp'],
-    input: 'index.js',
-    output: {
-        // file: 'dist/index.mjs',
-        dir: 'dist/',
-        // preserveModules: true,
-        format: 'esm',
-        // sourcemap: true,
-        globals: {
-            wp: 'wp',
-        },
-    },
+const babelConfig = {
+    babelrc: false,
+    exclude: [/\/core-js\//, 'node_modules/**'],
+    babelHelpers: 'runtime',
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                useBuiltIns: 'usage',
+                corejs: 3,
+            },
+        ],
+    ],
     plugins: [
-        resolve(),
-        babel({
-            babelrc: false,
-            exclude: 'node_modules/**',
-            sourceMaps: true,
-            inputSourceMap: true,
-            babelHelpers: 'bundled',
-            presets: [
-                ["@babel/preset-env"],
-            ],
-            plugins: [
-                "@babel/plugin-transform-react-jsx",
-            ]
-        }),
-        commonjs(),
+        '@babel/plugin-transform-react-jsx',
+        '@babel/plugin-transform-runtime',
     ],
 };
+
+export default [
+    {
+        external: ['wp', /@babel\/runtime/],
+        input: 'index.js',
+        output: {
+            file: 'dist/index.js',
+            format: 'esm',
+            globals: {
+                wp: 'wp',
+            },
+        },
+        plugins: [resolve(), babel(babelConfig), commonjs()],
+    },
+    {
+        external: [/@babel\/runtime/],
+        input: 'icons/material-design-icons.js',
+        output: {
+            file: 'dist/icons.js',
+            format: 'esm',
+        },
+        plugins: [resolve(), babel(babelConfig), commonjs()],
+    },
+];
